f551e684b2d1cf281238833391f5ebfa0872aab4
Adrienbrl <bruel.adrienpro@gmail.com>
2023-09-20 20:39:51 +0200

    Problème réglé pour les filtres. Les projets sont désormais triés suivant la bonne catégorie

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,19 +1,22 @@
-window.onload = () => {
-    let filters = document.querySelectorAll(".filters button");
+const filtersGallery = document.querySelectorAll("#projets .filters ul li");
+const imageGallery = document.querySelectorAll("#projets .gallery img");
+const all = document.querySelector(".all");
 
-    for(let filter of filters){
-        filter.addEventListener("click", function(){
-            let tag = this.classList[0];
-
-            let images = document.querySelectorAll(".gallery img");
-
-            for(let image of images){
-                image.classList.replace("active", "inactive");
-
-                if (image.classList.contains(tag) || tag === "all") {
-                    image.classList.replace("inactive", "active");
-                }
+for (let filter of filtersGallery) {
+    filter.addEventListener("click", function () {
+        for (let filter of filtersGallery) {
+            filter.classList.remove("active-tag");
+        }
+        filter.classList.add("active-tag");
+        for (let image of imageGallery) {
+            if (
+                filter.getAttribute("data-gallery-tag") === image.getAttribute("data-gallery-tag") || filter === all
+            ) {
+                image.style.display = "block";
+            } else {
+                image.style.display = "none"
             }
-        });
-    }
+        }
+
+    })
 }
