3634396eaea3265bfd2c77f982dfa03f95dd2977
Adrienbrl <bruel.adrienpro@gmail.com>
2023-10-03 18:55:49 +0200

    Ajout des cercles en fond sur les icones de la section compétences et ajout d'animations sur la barre de nav

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -10,11 +10,47 @@ for (let filter of filtersGallery) {
         filter.classList.add("active-tag");
         for (let image of imageGallery) {
             const tag = image.getAttribute("data-gallery-tag");
+            
             const shouldDisplay = tag.includes(filter.getAttribute("data-gallery-tag")) || filter === all;
             image.style.display = shouldDisplay ? "block" : "none";
+
             const galleryItem = image.parentElement; 
             galleryItem.style.display = shouldDisplay ? "block" : "none";
         }
     });
 }
+
+
+
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
+
+function handleIndicator(el) {
+  // Boucler sur tous items -> retirer la classe "is-active"
+  items.forEach(item => {
+    item.classList.remove('is-active');
+    item.removeAttribute('style');
+  })
+  
+  const elementColor = el.dataset.activeColor;
+  const target = el.dataset.target;
+  
+  console.log(target);
+  
+  // Styliser l'indicateur
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.backgroundColor = elementColor;
+  indicator.style.left = `${el.offsetLeft}px`;
+  
+  // Ajout la classe is-active
+  el.classList.add('is-active');
+  el.style.color = elementColor;
+}
+
+items.forEach((item, index) => {
+  item.addEventListener('click', e => {
+    handleIndicator(e.target)
+  });
+  item.classList.contains('is-active') && handleIndicator(item);
+});
 
