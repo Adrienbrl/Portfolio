c5f5e22d61270a52959c3c2dd189063670669cf0
Adrien Bruel <bruel.adrienpro@gmail.com>
2023-10-19 16:27:06 +0200

    Correction d'un bug sur la scroll bar, mise en place des modales pour la section projets. Dernières modifications la description des modales

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -107,3 +107,40 @@ btn.addEventListener('click', () => {
 })
 
 
+    // Sélectionnez tous les boutons "Voir plus"
+    const viewMoreButtons = document.querySelectorAll('.gallery-item button');
+
+    // Ajoutez un gestionnaire d'événements à chaque bouton
+    viewMoreButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Récupérez l'élément parent .gallery-item
+            const galleryItem = button.closest('.gallery-item');
+
+            // Récupérez l'attribut data-gallery-tag de l'image
+            const galleryTag = galleryItem.querySelector('img').getAttribute('data-gallery-tag');
+
+            // En fonction de la valeur de data-gallery-tag, affichez la modale appropriée
+            if (galleryTag === 'Html&Css') {
+                document.getElementById('modal-booki').style.display = 'block';
+            } else if (galleryTag === 'JavaScript') {
+                document.getElementById('modal-sophie').style.display = 'block';
+            } else if (galleryTag === 'JavaScript Html&Css') {
+                document.getElementById('modal-nina').style.display = 'block';
+            } else if (galleryTag === 'React') {
+                document.getElementById('modal-kasa').style.display = 'block';
+            }
+        });
+    });
+
+    // Gérez la fermeture des modales en ajoutant un gestionnaire d'événements pour les boutons "Fermer"
+    const closeButtons = document.querySelectorAll('.close-modal');
+    closeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Masquez la modale actuelle
+            button.closest('.modal').style.display = 'none';
+        });
+    });
+
+
+
+
